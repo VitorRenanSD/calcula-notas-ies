9bcdc5b350a69fca3a1bc4ca5d9885a61ed8d390
Vitor <167242612+VitorRenanSD@users.noreply.github.com>
2024-11-27 13:41:45 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,25 @@
 public class Main {
     public static void main(String[] args) {
 
-        Materia materia = new Materia("POO", 10.0f, 2.0f, 5f);
+        Materia poo = new Materia("Programacao Orientada a Objetos", 10.0f, 9.0f, 0f);
+        Materia par = new Materia("Paradigmas de Linguagens", 10.0f, 7.0f, 0f);
+        Materia hom = new Materia("Homem e Sociedade", 10.0f, 6.0f, 0f);
+        Materia geo = new Materia("Geometria Analitica", 10.0f, 2.0f, 0f);
+        Materia est = new Materia("Estatistica e Probabilidade", 10.0f, 7.0f, 0f);
+        Materia sql = new Materia("Banco de Dados", 8.5f, 10.0f, 0f);
+        Materia aps = new Materia("APS Roblox", 9.0f, 9.0f, 0f);
+        Materia alg = new Materia("Algebra Linear", 6.4f, 7.5f, 0f);
         Calculo calculo = new Calculo();
 
 
-        System.out.println(calculo.avaliar(materia));
-        
+        System.out.println(calculo.avaliar(poo));
+        System.out.println(calculo.avaliar(par));
+        System.out.println(calculo.avaliar(hom));
+        System.out.println(calculo.avaliar(geo));
+        System.out.println(calculo.avaliar(est));
+        System.out.println(calculo.avaliar(sql));
+        System.out.println(calculo.avaliar(aps));
+        System.out.println(calculo.avaliar(alg));
+
     }
 }
